2649684482b81f9b494a5a78f99663e42b309d05
CongLuanTran <congluan.tran19@gmail.com>
2025-01-01 23:00:52 +0700

    complete phonebook step 5

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,57 @@
 import { useState } from "react";
 
+const Filter = ({ value, onChange }) => (
+  <div>
+    filter shown with <input value={value} onChange={onChange} />
+  </div>
+);
+
+const PersonForm = ({ list, setter }) => {
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const handleNewName = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const handleNewNumber = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const person = {
+      name: newName,
+      number: newNumber,
+      id: list.length + 1,
+    };
+    if (list.some((p) => p.name === person.name)) {
+      alert(`${person.name} is already in the phonebook`);
+    } else {
+      console.log(`Added ${newName}`);
+      setter(list.concat(person));
+    }
+    setNewName("");
+    setNewNumber("");
+  };
+
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNewName} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNewNumber} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
 const Person = ({ person }) => (
   <div>
     {person.name} {person.number}
@@ -9,7 +61,9 @@ const Person = ({ person }) => (
 const Persons = ({ persons, filter }) => (
   <div>
     {persons
-      .filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
+      .filter((person) =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      )
       .map((person, id) => (
         <Person key={id} person={person} />
       ))}
@@ -23,62 +77,21 @@ const App = () => {
     { name: "Dan Abramov", number: "12-43-234345", id: 3 },
     { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [newSearch, setNewSearch] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
-  const handleNewName = (event) => {
+  const handleNewFilter = (event) => {
     console.log(event.target.value);
-    setNewName(event.target.value);
-  };
-
-  const handleNewNumber = (event) => {
-    console.log(event.target.value);
-    setNewNumber(event.target.value);
-  };
-
-  const handleNewSearch = (event) => {
-    console.log(event.target.value);
-    setNewSearch(event.target.value);
-  };
-
-  const addPerson = (event) => {
-    event.preventDefault();
-    const person = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1,
-    };
-    if (persons.some((p) => p.name === person.name)) {
-      alert(`${person.name} is already in the phonebook`);
-    } else {
-      console.log(`Added ${newName}`);
-      setPersons(persons.concat(person));
-    }
-    setNewName("");
-    setNewNumber("");
+    setNewFilter(event.target.value);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={newSearch} onChange={handleNewSearch} />
-      </div>
-      <h3>add a new</h3>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNewName} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNewNumber} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter value={newFilter} onChange={handleNewFilter} />
+      <h3>Add a new</h3>
+      <PersonForm list={persons} setter={setPersons}/>
       <h3>Numbers</h3>
-      <Persons persons={persons} filter={newSearch} />
+      <Persons persons={persons} filter={newFilter} />
     </div>
   );
 };
