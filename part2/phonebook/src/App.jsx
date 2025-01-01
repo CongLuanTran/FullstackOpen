631bb9e6f2034fc820b3403acf4e2ce8b131b6ae
CongLuanTran <congluan.tran19@gmail.com>
2025-01-01 22:42:10 +0700

    complete step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,15 +6,26 @@ const Person = ({ person }) => (
   </div>
 );
 
-const Persons = ({ persons }) =>
-  persons.map((person, id) => <Person key={id} person={person} />);
+const Persons = ({ persons, filter }) => (
+  <div>
+    {persons
+      .filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
+      .map((person, id) => (
+        <Person key={id} person={person} />
+      ))}
+  </div>
+);
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newSearch, setNewSearch] = useState("");
 
   const handleNewName = (event) => {
     console.log(event.target.value);
@@ -26,11 +37,17 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleNewSearch = (event) => {
+    console.log(event.target.value);
+    setNewSearch(event.target.value);
+  };
+
   const addPerson = (event) => {
     event.preventDefault();
     const person = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1,
     };
     if (persons.some((p) => p.name === person.name)) {
       alert(`${person.name} is already in the phonebook`);
@@ -45,6 +62,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={newSearch} onChange={handleNewSearch} />
+      </div>
+      <h3>add a new</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNewName} />
@@ -56,10 +77,8 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <div>
-        <Persons persons={persons} />
-      </div>
+      <h3>Numbers</h3>
+      <Persons persons={persons} filter={newSearch} />
     </div>
   );
 };
