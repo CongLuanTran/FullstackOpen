295f5e25180891cf4c5f1651de22cd9d1c865f33
Cong Luan Tran <congluan.tran19@gmail.com>
2025-01-11 19:25:20 +0700

    starting correction version

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -1,113 +1,88 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { Filter } from "./components/Filter";
 
-const Weather = ({ country }) => {
-  const [temp, setTemp] = useState();
-  const [wind, setWind] = useState();
-  const [icon, setIcon] = useState();
-  const api_key = import.meta.env.VITE_API_KEY;
-  const lat = country.capitalInfo.latlng[0];
-  const lon = country.capitalInfo.latlng[1];
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`;
+const Filter = ({ value, handleChange }) => (
+  <div>
+    find countries{" "}
+    <input
+      value={value}
+      onChange={handleChange}
+    />
+  </div>
+);
 
-  useEffect(() => {
-    console.log("getting temperature");
-    axios.get(url).then((response) => {
-      setTemp(response.data.main.temp);
-      setWind(response.data.wind.speed);
-      setIcon(response.data.weather[0].icon);
-      console.log("temparature obtained");
-    });
-  }, [url]);
+const Country = ({ country }) => {
+  const name = country.name.common;
+  const capital = country.capital[0];
+  const area = country.area;
+  const languages = Object.keys(country.languages);
+  const flagURL = country.flags.png;
 
   return (
-    <>
-      <h2>{`Weather in ${country.capital[0]}`}</h2>
-      <div>{`temperature ${temp} Celcius`}</div>
-      <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} />
-      <div>{`wind ${wind} m/s`}</div>
-    </>
+    <div>
+      <h2>{name}</h2>
+      <div>
+        capital {capital}
+      </div>
+      <div>
+        area {area}
+      </div>
+      <h4>languages</h4>
+      <ul>
+        {languages.map((key) => (
+          <li key={key}>{country.languages[key]}</li>
+        ))}
+      </ul>
+      <img src={flagURL} />
+    </div>
   );
 };
 
-const Country = ({ country }) => (
-  <>
-    <h2>{country.name.common}</h2>
-    <div>
-      <div>capital {country.capital[0]}</div>
-      <div>area {country.area}</div>
-    </div>
-    <h4>languages</h4>
-    <ul>
-      {Object.entries(country.languages).map(([key, value]) => (
-        <li key={key}>{value}</li>
-      ))}
-    </ul>
-    <img src={country.flags["png"]} alt={country.flags["alt"]} />
-    <Weather country={country} />
-  </>
-);
-
-const Countries = ({ countries, filter, setFilter }) => {
-  const matches = countries.filter((country) =>
-    country.name.common
-      .toLocaleLowerCase()
-      .includes(filter.toLocaleLowerCase()),
-  );
-
-  if (matches.length === 1) {
-    const country = matches[0];
-    return <Country country={country} />;
-  } else {
-    if (matches.length <= 10) {
-      return (
-        <div>
-          {matches.map((country) => (
-            <div key={country.cca2}>
-              {country.name.common}
-              <button value={country.name.common} onClick={setFilter}>
-                show
-              </button>
-            </div>
-          ))}
-        </div>
-      );
-    } else {
-      return <div>Too many matches, specify another filter</div>;
-    }
+const Countries = ({ countries, setShow }) => {
+  if (countries.length === 1) {
+    return <Country country={countries[0]} />;
   }
+
+  if (countries.length <= 10) {
+    return (
+      <div>
+        {countries.map((c) => (
+          <div key={c.fifa}>
+            {c.name.common}{" "}
+            <button onClick={() => setShow(c.name.common)}>show</button>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return <div>Too many matches, specify another filter</div>;
 };
 
 const App = () => {
   const [countries, setCountries] = useState([]);
-  const [newFilter, setNewFilter] = useState("");
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
-    console.log("effect");
     axios
       .get("https://studies.cs.helsinki.fi/restcountries/api/all")
-      .then((response) => {
-        setCountries(response.data);
-        console.log("promise fulfilled");
-      });
+      .then((response) => setCountries(response.data));
   }, []);
 
-  const handleNewFilter = (event) => {
-    console.log(event.target.value);
-    setNewFilter(event.target.value);
+  const handleFilterChange = ({ target }) => {
+    console.log(target.value);
+    setFilter(target.value);
   };
+
+  const matches = countries.filter((c) =>
+    c.name.common.toLowerCase().includes(filter.toLocaleLowerCase())
+  );
 
   return (
     <div>
-      <Filter value={newFilter} onChange={handleNewFilter} />
-      <Countries
-        countries={countries}
-        filter={newFilter}
-        setFilter={handleNewFilter}
-      />
+      <Filter value={filter} handleChange={handleFilterChange} />
+      <Countries countries={matches} setShow={setFilter} />
     </div>
   );
 };
-
 export default App;
--- part2/dataforcountries/src/components/Filter.jsx
+++
@@ -1,5 +0,0 @@
-export const Filter = ({ value, onChange }) => (
-  <div>
-    find countries <input value={value} onChange={onChange} />
-  </div>
-);
