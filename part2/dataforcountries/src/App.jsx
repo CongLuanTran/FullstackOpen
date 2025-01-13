e9a92544fdbeae526cc5e105a0e4c6a4b3a3bb6c
CongLuanTran <congluan.tran19@gmail.com>
2025-01-13 15:18:50 +0700

    finish refactoring data for countries

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -3,30 +3,36 @@ import axios from "axios";
 
 const Filter = ({ value, handleChange }) => (
   <div>
-    find countries{" "}
-    <input
-      value={value}
-      onChange={handleChange}
-    />
+    find countries <input value={value} onChange={handleChange} />
   </div>
 );
 
 const Country = ({ country }) => {
+  const [weather, setWeather] = useState(null);
+
   const name = country.name.common;
-  const capital = country.capital[0];
+  const [capital] = country.capital;
   const area = country.area;
   const languages = Object.keys(country.languages);
   const flagURL = country.flags.png;
 
+  useEffect(() => {
+    const api_key = import.meta.env.VITE_SOME_KEY;
+    const [lat, lon] = country.capitalInfo.latlng;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`;
+    axios.get(url).then(({ data }) => setWeather(data));
+  }, [country]);
+
+  if (!weather) return null;
+
+  const icon = weather.weather[0].icon;
+  const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
   return (
     <div>
       <h2>{name}</h2>
-      <div>
-        capital {capital}
-      </div>
-      <div>
-        area {area}
-      </div>
+      <div>capital {capital}</div>
+      <div>area {area}</div>
       <h4>languages</h4>
       <ul>
         {languages.map((key) => (
@@ -34,6 +40,10 @@ const Country = ({ country }) => {
         ))}
       </ul>
       <img src={flagURL} />
+      <h3>Weather in {capital}</h3>
+      <div>temperature {weather.main.temp} Celcius</div>
+      <img src={iconUrl} alt={weather.weather.description} />
+      <div>wind {weather.wind.speed} m/s</div>
     </div>
   );
 };
@@ -47,7 +57,7 @@ const Countries = ({ countries, setShow }) => {
     return (
       <div>
         {countries.map((c) => (
-          <div key={c.fifa}>
+          <div key={c.ccn3}>
             {c.name.common}{" "}
             <button onClick={() => setShow(c.name.common)}>show</button>
           </div>
@@ -75,7 +85,7 @@ const App = () => {
   };
 
   const matches = countries.filter((c) =>
-    c.name.common.toLowerCase().includes(filter.toLocaleLowerCase())
+    c.name.common.toLowerCase().includes(filter.toLocaleLowerCase()),
   );
 
   return (
