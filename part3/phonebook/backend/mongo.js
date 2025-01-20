15e75bd921c4e3c83b8cc9371688c26eec366292
CongLuanTran <congluan.tran19@gmail.com>
2025-01-20 23:11:26 +0700

    add mongoDB to backend

---
+++ part3/phonebook/backend/mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://calsun13:${password}@fullstackopen0.fhoqd.mongodb.net/phonebook?retryWrites=true&w=majority&appName=FullstackOpen0`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const schema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", schema);
+
+if (process.argv.length == 3) {
+  Person.find({}).then((persons) => {
+    console.log("phonebook:");
+    persons.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else if (process.argv.length == 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+  person.save().then((result) => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  console.log("invalid argument");
+}
