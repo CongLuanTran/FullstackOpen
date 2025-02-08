632ed73dfad2e833921615170b81e40768a2f063
Cong Luan Tran <congluan.tran19@gmail.com>
2025-02-08 20:32:30 +0700

    reformat API test file

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -35,13 +35,13 @@ describe('when there are some blogs saved initially', () => {
     assert.strictEqual(response.body.length, helper.initialBlogs.length)
   })
 
-  test('a blog is about `First class tests`', async () => {
+  test('contain a blog about `First class tests`', async () => {
     const response = await api.get('/api/blogs')
     const titles = response.body.map(e => e.title)
     assert(titles.includes('First class tests'))
   })
 
-  describe.only('viewing a specific blog', () => {
+  describe('viewing a specific blog', () => {
     test('should success with a valid id', async () => {
       const allBlogs = await helper.blogsInDb()
       const firstBlog = allBlogs[0]
@@ -54,7 +54,7 @@ describe('when there are some blogs saved initially', () => {
       assert.deepStrictEqual(resultBlog.body, firstBlog)
     })
 
-    test('should return 404 for non-existing but valid IDs', async () => {
+    test('should fail with status code 404 for non-existing but valid IDs', async () => {
       const validNonExistingId = await helper.nonExistingId()
 
       await api
@@ -62,7 +62,7 @@ describe('when there are some blogs saved initially', () => {
         .expect(404)
     })
 
-    test('should return 400 for invalid IDs', async () => {
+    test('should should fail with status code 400 for invalid IDs', async () => {
       const invalidId = '8290482904829048989'
 
       await api
@@ -71,66 +71,10 @@ describe('when there are some blogs saved initially', () => {
     })
   })
 
-  test('a valid blog can be added', async () => {
-    const newBlog = {
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      likes: 5,
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-    const response = await api.get('/api/blogs')
-
-    const titles = response.body.map(r => r.title)
-
-    assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
-
-    assert(titles.includes('Go To Statement Considered Harmful'))
-  })
-
-  test('blog without like default to 0 like', async () => {
-    const noLike = {
-      title: 'React patterns',
-      author: 'Michael Chan',
-      url: 'https://reactpatterns.com/',
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(noLike)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-    const response = await api.get('/api/blogs')
-
-    const blogs = response.body
-
-    assert.strictEqual(blogs.length, helper.initialBlogs.length + 1)
-
-    assert(blogs.find(blog => blog.title === 'React patterns').likes === 0)
-  })
-
-  describe('posting blogs without title or url return 400', () => {
-
-    test('blog without url', async () => {
-      const badBlog = {
+  describe('addition of a blog', () => {
+    test('should success if the blog is valid', async () => {
+      const newBlog = {
         title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        likes: 5,
-      }
-
-      await api
-        .post('/api/blogs')
-        .send(badBlog)
-        .expect(400)
-    })
-
-    test('blog without title', async () => {
-      const badBlog = {
         author: 'Edsger W. Dijkstra',
         url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
         likes: 5,
@@ -138,20 +82,76 @@ describe('when there are some blogs saved initially', () => {
 
       await api
         .post('/api/blogs')
-        .send(badBlog)
-        .expect(400)
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const response = await api.get('/api/blogs')
+      const titles = response.body.map(r => r.title)
+
+      assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+      assert(titles.includes('Go To Statement Considered Harmful'))
     })
 
-    test('blog without both', async () => {
-      const badBlog = {
-        author: 'Edsger W. Dijkstra',
-        likes: 5,
+    describe('should fail with status code 400 if title or url is empty', () => {
+
+      test('blog without url', async () => {
+        const badBlog = {
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          likes: 5,
+        }
+
+        await api
+          .post('/api/blogs')
+          .send(badBlog)
+          .expect(400)
+      })
+
+      test('blog without title', async () => {
+        const badBlog = {
+          author: 'Edsger W. Dijkstra',
+          url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+          likes: 5,
+        }
+
+        await api
+          .post('/api/blogs')
+          .send(badBlog)
+          .expect(400)
+      })
+
+      test('blog without both', async () => {
+        const badBlog = {
+          author: 'Edsger W. Dijkstra',
+          likes: 5,
+        }
+
+        await api
+          .post('/api/blogs')
+          .send(badBlog)
+          .expect(400)
+      })
+    })
+
+    test('should assign default value 0 to `likes` if it is missing', async () => {
+      const noLike = {
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
       }
 
       await api
         .post('/api/blogs')
-        .send(badBlog)
-        .expect(400)
+        .send(noLike)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const response = await api.get('/api/blogs')
+      const blogs = response.body
+
+      assert.strictEqual(blogs.length, helper.initialBlogs.length + 1)
+      assert(blogs.find(blog => blog.title === 'React patterns').likes === 0)
     })
   })
 })
