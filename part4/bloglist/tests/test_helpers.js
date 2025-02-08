ec5322d2a27486c68c90c52d5bce59ff96b994ea
CongLuanTran <congluan.tran19@gmail.com>
2025-02-08 09:31:22 +0700

    moving some test functionality to a seperate helper file

---
+++ part4/bloglist/tests/test_helpers.js
@@ -0,0 +1,17 @@
+const Note = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+  },
+  {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+  }
+]
+
