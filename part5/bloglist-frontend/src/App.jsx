3179e53eb396061ed37fb334f6f631aafd0890a0
Cong Luan Tran <congluan.tran19@gmail.com>
2025-02-21 10:16:15 +0700

    complete add blog utils

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -5,7 +5,10 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [notification, setNotification] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
@@ -43,9 +46,12 @@ const App = () => {
       setUsername('')
       setPassword('')
     } catch (exception) {
-      setErrorMessage('Wrong credentials')
+      setNotification({
+        message: 'wrong username or password',
+        isError: true,
+      })
       setTimeout(() => {
-        setErrorMessage(null)
+        setNotification(null)
       }, 5000)
     }
   }
@@ -55,9 +61,94 @@ const App = () => {
     setUser(null)
   }
 
+  const notify = (notification) => {
+    if (!notification) {
+      return null
+    }
+
+    const { message, isError } = notification
+    if (!message) {
+      return null
+    }
+
+    const notifyState = {
+      color: isError ? 'red' : 'green',
+      background: 'lightgrey',
+      fontSize: 20,
+      borderStyle: 'solid',
+      borderRadius: 5,
+      padding: 10,
+      marginBottom: 10,
+    }
+
+    return <div style={notifyState}>{message}</div>
+  }
+
+  const addNewBlog = async (event) => {
+    event.preventDefault()
+    try {
+      const blog = {
+        title: title,
+        author: author,
+        url: url,
+      }
+
+      const newBlog = await blogService.create(blog)
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      setBlogs(blogs.concat(newBlog))
+      setNotification({
+        message: `a new blog ${title} by ${author} added`
+      })
+    } catch (exception) {
+      setNotification({
+        message: 'invalid blog',
+        isError: true,
+      })
+    }
+  }
+
+  const blogForm = () => (
+    <>
+      <h2>create new</h2>
+      <form onSubmit={addNewBlog}>
+        <div>
+          title:
+          <input
+            type="text"
+            value={title}
+            name="Title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            type="text"
+            value={author}
+            name="Author"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url:
+          <input
+            type="text"
+            value={url}
+            name="Url"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </>
+  )
+
   const loginForm = () => (
     <>
       <h2>log in to application</h2>
+      {notify(notification)}
       <form onSubmit={handleLogin}>
         <div>
           username
@@ -65,7 +156,8 @@ const App = () => {
             type="text"
             value={username}
             name="Username"
-            onChange={({ target }) => setUsername(target.value)} />
+            onChange={({ target }) => setUsername(target.value)}
+          />
         </div>
         <div>
           password
@@ -73,7 +165,8 @@ const App = () => {
             type="password"
             value={password}
             name="Password"
-            onChange={({ target }) => setPassword(target.value)} />
+            onChange={({ target }) => setPassword(target.value)}
+          />
         </div>
         <button type="submit">login</button>
       </form>
@@ -83,10 +176,14 @@ const App = () => {
   const allBlogs = () => (
     <>
       <h2>blogs</h2>
+      {notify(notification)}
       <p>
         {user.name} logged-in
         <button onClick={handleLogout}>logout</button>
       </p>
+      <div>
+        {blogForm()}
+      </div>
       {
         blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
