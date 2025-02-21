eedff7f5afa8575b7127f65996d2e23c955c931d
Cong Luan Tran <congluan.tran19@gmail.com>
2025-02-21 07:50:32 +0700

    complete logout util

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -11,9 +11,19 @@ const App = () => {
   const [user, setUser] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
-    )
+    blogService
+      .getAll().then(blogs =>
+        setBlogs(blogs)
+      )
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
   }, [])
 
   const handleLogin = async (event) => {
@@ -23,6 +33,12 @@ const App = () => {
         username,
         password,
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser',
+        JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -32,6 +48,11 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
   }
 
   const loginForm = () => (
@@ -62,7 +83,10 @@ const App = () => {
   const allBlogs = () => (
     <>
       <h2>blogs</h2>
-      <p>{user.name} logged-in</p>
+      <p>
+        {user.name} logged-in
+        <button onClick={handleLogout}>logout</button>
+      </p>
       {
         blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
