8b2f5432ca45de42eef8cb3f001f95d94de4eea2
CongLuanTran <congluan.tran19@gmail.com>
2025-03-02 13:47:20 +0700

    complete blog details view

--- part5/bloglist/frontend/src/components/Blog.jsx
+++ part5/bloglist/frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBotton: 5,
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const displayStyle = { display: visible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+      <div style={displayStyle}>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes}
+          <button>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
