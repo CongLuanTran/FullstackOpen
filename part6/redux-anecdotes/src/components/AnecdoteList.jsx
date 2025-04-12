735fb0325afe040d13c002b9a39a04f92a82f48e
Cong Luan Tran <congluan.tran19@gmail.com>
2025-04-12 16:48:35 +0700

    move anecdotes list to a separate component

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,44 @@
+import { useDispatch, useSelector } from "react-redux"
+import { voteFor } from "../reducers/anecdoteReducer"
+import PropTypes from 'prop-types'
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <div>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+Anecdote.propTypes = {
+  anecdote: PropTypes.object,
+  handleClick: PropTypes.func
+}
+
+const AnecdoteList = () => {
+
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  return (
+    <>
+      {anecdotes.sort((a, b) => b.votes - a.votes)
+        .map(anecdote =>
+          <Anecdote
+            key={anecdote.id}
+            anecdote={anecdote}
+            handleClick={() => dispatch(voteFor(anecdote.id))}
+          />
+        )
+      }
+    </>
+  )
+}
+
+export default AnecdoteList
