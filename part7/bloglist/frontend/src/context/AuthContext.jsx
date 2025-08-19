9090520f24f1267078a3149ce425366258765535
Cong Luan Tran <congluan.tran19@gmail.com>
2025-08-19 22:29:05 +0700

    part 7: switch to useReducer because the assignment say so

--- part7/bloglist/frontend/src/context/AuthContext.jsx
+++ part7/bloglist/frontend/src/context/AuthContext.jsx
@@ -1,23 +1,34 @@
-import { createContext, useContext, useState, useEffect } from 'react'
+import { createContext, useContext, useEffect, useReducer } from 'react'
 import loginService from '../services/login'
 import storage from '../services/storage'
 import { useNotification } from './NotificationContext'
 
+const UserReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.payload
+    case 'CLEAR':
+      return null
+    default:
+      return state
+  }
+}
+
 const AuthContext = createContext()
 
 export const AuthContextProvider = (props) => {
-  const [user, setUser] = useState(null)
+  const [user, userDispatch] = useReducer(UserReducer, null)
   const { notify } = useNotification()
 
   useEffect(() => {
     const user = storage.loadUser()
-    if (user) setUser(user)
+    if (user) userDispatch({ type: 'SET', payload: user })
   }, [])
 
   const handleLogin = async (credentials) => {
     try {
       const user = await loginService.login(credentials)
-      setUser(user)
+      userDispatch({ type: 'SET', payload: user })
       storage.saveUser(user)
       notify(`Welcome back, ${user.name}`)
     } catch (error) {
@@ -26,7 +37,7 @@ export const AuthContextProvider = (props) => {
   }
 
   const handleLogout = () => {
-    setUser(null)
+    userDispatch({ type: 'CLEAR' })
     storage.removeUser()
     notify(`Bye, ${user.name}!`)
   }
--- part7/bloglist/frontend/src/context/NotificationContext.jsx
+++ part7/bloglist/frontend/src/context/NotificationContext.jsx
@@ -1,14 +1,28 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useReducer } from 'react'
+
+const notificationReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.payload
+    case 'CLEAR':
+      return null
+    default:
+      return state
+  }
+}
 
 const NotificationContext = createContext()
 
 export const NotificationContextProvider = (props) => {
-  const [notification, setNotification] = useState(null)
+  const [notification, notificationDispatch] = useReducer(
+    notificationReducer,
+    null
+  )
 
   const notify = (message, type = 'success') => {
-    setNotification({ message, type })
+    notificationDispatch({ type: 'SET', payload: { message, type } })
     setTimeout(() => {
-      setNotification(null)
+      notificationDispatch({ type: 'CLEAR' })
     }, 5000)
   }
 
