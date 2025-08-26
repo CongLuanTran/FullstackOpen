94f9bf73f77ffc0b33c6c43995158bd3a50faf56
Cong Luan Tran <congluan.tran19@gmail.com>
2025-08-27 00:47:29 +0700

    feat(part7-views): add Link to users view to navigate to individual user view

--- part7/bloglist/frontend/src/components/UserList.jsx
+++ part7/bloglist/frontend/src/components/UserList.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 import userService from '../services/users'
+import { Link } from 'react-router-dom'
 
 const UserList = () => {
   const result = useQuery({
@@ -18,16 +19,22 @@ const UserList = () => {
     <div>
       <h2>Users</h2>
       <table>
-        <tr>
-          <th></th>
-          <th>blogs created</th>
-        </tr>
-        {[...users].sort(byBlogs).map((user) => (
+        <thead>
           <tr>
-            <td>{user.name}</td>
-            <td>{user.blogs.length}</td>
+            <th></th>
+            <th>blogs created</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {[...users].sort(byBlogs).map((user) => (
+            <tr key={user.id}>
+              <td>
+                <Link to={`/users/${user.id}`}>{user.name}</Link>
+              </td>
+              <td>{user.blogs.length}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   )
