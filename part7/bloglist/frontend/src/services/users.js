a9ef7bc65a34457bcfde12771f4be2ea5602a89e
Cong Luan Tran <congluan.tran19@gmail.com>
2025-08-27 00:43:43 +0700

    fix(part7-views): remove unused getConfit in user service

--- part7/bloglist/frontend/src/services/users.js
+++ part7/bloglist/frontend/src/services/users.js
@@ -1,11 +1,6 @@
 import axios from 'axios'
-import storage from './storage'
 
 const baseUrl = '/api/users'
-
-const getConfit = () => ({
-  headers: { Authorization: `Bearer ${storage.loadUser().token}` },
-})
 
 const getAll = async () => {
   const response = await axios.get(baseUrl)
