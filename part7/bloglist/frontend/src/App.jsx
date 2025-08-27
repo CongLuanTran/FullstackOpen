f2dc58386d715404f84337fa6bfcdd454f336fa5
Cong Luan Tran <congluan.tran19@gmail.com>
2025-08-27 12:03:28 +0700

    feat(part7-views): add navigation links and move them to top of the page

--- part7/bloglist/frontend/src/App.jsx
+++ part7/bloglist/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import BlogList from './components/BlogList'
 import Login from './components/Login'
 import NewBlog from './components/NewBlog'
@@ -21,14 +21,26 @@ const App = () => {
     )
   }
 
+  const padding = {
+    padding: 5,
+  }
+
   return (
     <Router>
+      <nav>
+        <Link style={padding} to="/">
+          blogs
+        </Link>
+        <Link style={padding} to="/users">
+          users
+        </Link>
+        <span style={padding}>{user.name} logged in</span>
+        <button style={padding} onClick={handleLogout}>
+          logout
+        </button>
+      </nav>
       <h2>blogs</h2>
       <Notification />
-      <div>
-        {user.name} logged in
-        <button onClick={handleLogout}>logout</button>
-      </div>
       <Routes>
         <Route
           path="/"
